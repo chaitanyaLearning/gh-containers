8a5eaeb1bae18ccb589d0d056dee860be6b78774
chaithu117-dev <gchaitanya1903@gmail.com>
2025-07-22 23:54:06 +0530

    trial 5

--- app.js
+++ app.js
@@ -9,20 +9,4 @@ app.use(bodyParser.json());
 
 app.use(eventRoutes);
 
-// app.listen(process.env.PORT);
-const client = MongoClient(uri);
-client.connect()
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-        });
-    })
-    .catch(err => {
-        console.error("DB connection failed:", err.message);
-        process.exit(1);
-    });
-
-process.on('unhandledRejection', err => {
-    console.error('Unhandled Rejection:', err);
-    process.exit(1);
-});
+app.listen(process.env.PORT);
